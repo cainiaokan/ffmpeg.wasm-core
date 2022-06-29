87e341e9ec4fcc9f3ff2edbda887b70bfceefdfb
louielang <louielang@tencent.com>
2022-06-29 14:11:50 +0800

    更新配置

--- wasm/build-scripts/build-ffmpeg.sh
+++ wasm/build-scripts/build-ffmpeg.sh
@@ -7,9 +7,9 @@ mkdir -p wasm/dist
 emmake make -j
 FLAGS=(
   -I. -I./fftools -I$BUILD_DIR/include
-  -Llibavcodec -Llibavdevice -Llibavfilter -Llibavformat -Llibavresample -Llibavutil -Lharfbuzz -Llibass -Lfribidi -Llibpostproc -Llibswscale -Llibswresample -L$BUILD_DIR/lib
+  -Llibavcodec -Llibavdevice -Llibavfilter -Llibavformat -Llibavresample -Llibavutil -Llibpostproc -Llibswscale -Llibswresample -L$BUILD_DIR/lib
   -Wno-deprecated-declarations -Wno-pointer-sign -Wno-implicit-int-float-conversion -Wno-switch -Wno-parentheses -Qunused-arguments
-  -lavdevice -lavfilter -lavformat -lavcodec -lswresample -lswscale -lavutil -lpostproc -lm -lharfbuzz -lfribidi -llibass -lx264 -lx265 -lvpx -lwavpack -lmp3lame -lfdk-aac -lvorbis -lvorbisenc -lvorbisfile -logg -ltheora -ltheoraenc -ltheoradec -lz -lfreetype -lopus -lwebp -pthread
+  -lavdevice -lavfilter -lavformat -lavcodec -lswresample -lswscale -lavutil -lpostproc -lm -lwavpack -lmp3lame -lfdk-aac -lvorbis -lvorbisenc -lvorbisfile -logg -lz -lopus -pthread
   fftools/ffmpeg_opt.c fftools/ffmpeg_filter.c fftools/ffmpeg_hw.c fftools/cmdutils.c fftools/ffmpeg.c
   -o wasm/dist/ffmpeg-core.js
   -s USE_SDL=2                                  # use SDL2
@@ -21,9 +21,28 @@ FLAGS=(
   -s EXPORT_NAME="createFFmpegCore"             # assign export name for browser
   -s EXPORTED_FUNCTIONS="[_main, _proxy_main]"  # export main and proxy_main funcs
   -s EXTRA_EXPORTED_RUNTIME_METHODS="[FS, cwrap, ccall, setValue, writeAsciiToMemory]"   # export preamble funcs
-  -s INITIAL_MEMORY=2146435072                  # 64 KB * 1024 * 16 * 2047 = 2146435072 bytes ~= 2 GB
+  -s INITIAL_MEMORY=1073217536                  # 64 KB * 1024 * 16 * 2047 = 2146435072 bytes ~= 2 GB
   --post-js wasm/post-js.js
   $OPTIM_FLAGS
 )
+# FLAGS=(
+#   -I. -I./fftools -I$BUILD_DIR/include
+#   -Llibavcodec -Llibavdevice -Llibavfilter -Llibavformat -Llibavresample -Llibavutil -Lharfbuzz -Llibass -Lfribidi -Llibpostproc -Llibswscale -Llibswresample -L$BUILD_DIR/lib
+#   -Wno-deprecated-declarations -Wno-pointer-sign -Wno-implicit-int-float-conversion -Wno-switch -Wno-parentheses -Qunused-arguments
+#   -lavdevice -lavfilter -lavformat -lavcodec -lswresample -lswscale -lavutil -lpostproc -lm -lharfbuzz -lfribidi -lass -lx264 -lx265 -lvpx -lwavpack -lmp3lame -lfdk-aac -lvorbis -lvorbisenc -lvorbisfile -logg -ltheora -ltheoraenc -ltheoradec -lz -lfreetype -lopus -lwebp
+#   fftools/ffmpeg_opt.c fftools/ffmpeg_filter.c fftools/ffmpeg_hw.c fftools/cmdutils.c fftools/ffmpeg.c
+#   -s USE_SDL=2                                  # use SDL2
+#   -s INVOKE_RUN=0                               # not to run the main() in the beginning
+#   -s EXIT_RUNTIME=1                             # exit runtime after execution
+#   -s MODULARIZE=1                               # use modularized version to be more flexible
+#   -s EXPORT_NAME="createFFmpegCore"             # assign export name for browser
+#   -s EXPORTED_FUNCTIONS="[_main]"  # export main and proxy_main funcs
+#   -s EXPORTED_RUNTIME_METHODS="[FS, cwrap, ccall, setValue, writeAsciiToMemory]"   # export preamble funcs
+#   -s INITIAL_MEMORY=2146435072                  # 64 KB * 1024 * 16 * 2047 = 2146435072 bytes ~= 2 GB
+#   --pre-js wasm/src/pre.js
+#   --post-js wasm/src/post.js
+#   $OPTIM_FLAGS
+#   ${EXTRA_FLAGS[@]}
+# )
 echo "FFMPEG_EM_FLAGS=${FLAGS[@]}"
 emcc "${FLAGS[@]}"
--- wasm/build-scripts/configure-ffmpeg.sh
+++ wasm/build-scripts/configure-ffmpeg.sh
@@ -5,22 +5,36 @@ source $(dirname $0)/var.sh
 
 FLAGS=(
   "${FFMPEG_CONFIG_FLAGS_BASE[@]}"
-  --enable-gpl            # required by x264
+  --disable-network
+  --disable-devices
+  --enable-small
+  --disable-muxers
+  --disable-demuxers
+  --disable-encoders
+  --disable-decoders
+  
+  --enable-muxer=mp3,flv,mov,mp4,pcm_s16le,wav
+  --enable-demuxer=aac,flac,flv,mov,mp4,mp3,ogg,pcm_s16le,wav
+  --enable-encoder=aac,libmp3lame,pcm_s16le,wavpack
+  --enable-decoder=aac,alac,flac,libmp3lame,opus,libopus,vorbis,libvorbis,pcm_s16le,wavpack
+  --enable-protocol=file
+  
+  # --enable-gpl            # required by x264
   --enable-nonfree        # required by fdk-aac
   --enable-zlib           # enable zlib
-  --enable-libx264        # enable x264
-  --enable-libx265        # enable x265
-  --enable-libvpx         # enable libvpx / webm
+  # --enable-libx264        # enable x264
+  # --enable-libx265        # enable x265
+  # --enable-libvpx         # enable libvpx / webm
   --enable-libwavpack     # enable libwavpack
   --enable-libmp3lame     # enable libmp3lame
   --enable-libfdk-aac     # enable libfdk-aac
-  --enable-libtheora      # enable libtheora
+  # --enable-libtheora      # enable libtheora
   --enable-libvorbis      # enable libvorbis
-  --enable-libfreetype    # enable freetype
+  # --enable-libfreetype    # enable freetype
   --enable-libopus        # enable opus
-  --enable-libwebp        # enable libwebp
-  --enable-libass         # enable libass
-  --enable-libfribidi     # enable libfribidi
+  # --enable-libwebp        # enable libwebp
+  # --enable-libass         # enable libass
+  # --enable-libfribidi     # enable libfribidi
   # --enable-libaom         # enable libaom
 )
 echo "FFMPEG_CONFIG_FLAGS=${FLAGS[@]}"
